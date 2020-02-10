33798c1d4e5a4ac5d37aa619e668937fabda7375
Madhu-nannuri <nannurimadhugeetha@gmail.com>
2020-02-10 23:36:56 +0530

    commiting a text file

---
+++ epam/src/main/java/week3/calculator/app.java
@@ -0,0 +1,14 @@
+package week3.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Application app=new Application();
+        app.start();
+    }
+}
